c67020505b5b0295edbfe977b89117d4e709c3ca
DavidNNussbaum <brocknynj@aol.com>
2021-01-07 17:37:40 -0500

    commit tree e7dc10b78cd6fa79d213f676e26fa69d9cb4061f

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,25 @@
+require 'pry'
 class Item
   attr_accessor :name, :price
+  @@all = []
 
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self
+  end
+
+  def self.everything
+    @@all
+  end
+
+  def self.price(name)
+    if item = Item.everything.find do |i|
+       i.name == name
+    end
+    item.price
+  else
+    return nil
+  end
   end
 end
